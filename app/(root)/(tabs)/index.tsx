9ae39405d026fa17564381ca232ce9a2f8d92d7e
Sandesh <sandesh@lumosys.in>
2025-01-16 23:39:13 +0530

    Create Profile page with Logout functionality and add Home page

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,16 +1,27 @@
+import Search from "@/components/Search";
+import icons from "@/constants/icons";
+import images from "@/constants/images";
 import { Link } from "expo-router";
-import { Text, View } from "react-native";
+import { Image, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text className="font-bold my-10 text-red-400 font-rubik text-3xl">Welcome  to Real-Estate</Text>
+<SafeAreaView className="bg-white h-full">
+  <View className="px-5">
+    <View className="flex flex-row items-center justify-between mt-5">
+      <View className="flex flex-row items-center">
+        <Image source={images.avatar} className="size-12 rounded-full"/>
+        <View className="flex flex-col items-start ml-2 justify-center">
+          <Text className="text-xs font-rubik text-black-100">Good Morning</Text>
+          <Text className="text-base font-rubik-medium text-black-300">Sandesh</Text>
+        </View>
+      </View>
+      <Image source={icons.bell} className="size-6" />
     </View>
+  </View>
+
+  <Search/>
+</SafeAreaView>
   );
 }
