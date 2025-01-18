1f22d821153b5bf89207ce9cf0b33bbdafd3c815
Sandesh <sandesh@lumosys.in>
2025-01-18 21:38:58 +0530

    add Card in Home page

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,27 +1,47 @@
+import { Card, FeaturedCard } from "@/components/Cards";
 import Search from "@/components/Search";
 import icons from "@/constants/icons";
 import images from "@/constants/images";
 import { Link } from "expo-router";
+import { TouchableOpacity } from "react-native";
 import { Image, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
   return (
-<SafeAreaView className="bg-white h-full">
-  <View className="px-5">
-    <View className="flex flex-row items-center justify-between mt-5">
-      <View className="flex flex-row items-center">
-        <Image source={images.avatar} className="size-12 rounded-full"/>
-        <View className="flex flex-col items-start ml-2 justify-center">
-          <Text className="text-xs font-rubik text-black-100">Good Morning</Text>
-          <Text className="text-base font-rubik-medium text-black-300">Sandesh</Text>
+    <SafeAreaView className="bg-white h-full">
+      <View className="px-5">
+        <View className="flex flex-row items-center justify-between mt-5">
+          <View className="flex flex-row items-center">
+            <Image source={images.avatar} className="size-12 rounded-full" />
+            <View className="flex flex-col items-start ml-2 justify-center">
+              <Text className="text-xs font-rubik text-black-100">
+                Good Morning
+              </Text>
+              <Text className="text-base font-rubik-medium text-black-300">
+                Sandesh
+              </Text>
+            </View>
+          </View>
+          <Image source={icons.bell} className="size-6" />
         </View>
-      </View>
-      <Image source={icons.bell} className="size-6" />
-    </View>
-  </View>
+        <Search />
 
-  <Search/>
-</SafeAreaView>
+        <View className="my-5">
+          <View className="flex flex-row items-center justify-between">
+            <Text className="text-xl font-rubik-bold text-black-300">
+              Featured
+            </Text>
+            <TouchableOpacity>
+              <Text className="text-base font-rubik-bold text-primary-300">
+                See All
+              </Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+        <FeaturedCard/>
+      <Card/>
+      </View>
+    </SafeAreaView>
   );
 }
